4b44be46266d07287009fba8040ddedeab7d7cf3
xieyangyuyue <1483618794@qq.com>
2025-02-20 19:22:00 +0800

    [update]修正测试代码导包问题

--- mygolang/mytest/Add.go
+++ mygolang/mytest/Add.go
@@ -1,6 +1,6 @@
-package math
+package main
 
 // Add 返回两个整数的和
 func Add(a, b int) int {
-    return a + b
+	return a + b
 }
---
+++ mygolang/mytest/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	result := Add(2, 3)
+	fmt.Println("2 + 3 =", result)
+}
